40703d675137ee06c3ec73b54b4cb8d1a4f48db6
Amin Febryansyah <aminfebryansyah240203@gmail.com>
2023-11-08 01:58:00 +0700

    Initial commit

---
+++ app/src/main/java/com/example/esjumbo2/data/SumberData.kt
@@ -0,0 +1,13 @@
+package com.example.esjumbo2.data
+
+import com.example.esjumbo2.R
+
+object SumberData {
+    val flavors = listOf(
+        R.string.natural,
+        R.string.vanilla,
+        R.string.lemon,
+        R.string.milk,
+        R.string.lcyhe
+    )
+}
